535361857c568e6154b8aa8aeb7dde69a5818266
qyuiq <popow2281337@gmail.com>
2026-09-25 00:02:27 +0700

    Расположил элементы

--- app/src/main/java/com/example/chronometer/MainActivity.kt
+++ app/src/main/java/com/example/chronometer/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.chronometer.ui.theme.ChronometerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +29,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ChronometerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    ChronometerScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +42,57 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun ChronometerScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+
+        Text(
+            text = "00:00",
+            fontSize = 48.sp
+        )
+
+        Spacer(
+            modifier = Modifier.height(40.dp)
+        )
+
+        Button(
+            onClick = { },
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier.width(100.dp)
+        ) {
+            Text("Start")
+        }
+
+        Button(
+            onClick = { },
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier.width(100.dp)
+        ) {
+            Text("Pause")
+        }
+
+        Button(
+            onClick = { },
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier.width(100.dp)
+        ) {
+            Text("Reset")
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ChronometerTheme {
-        Greeting("Android")
+        ChronometerScreen()
     }
 }
